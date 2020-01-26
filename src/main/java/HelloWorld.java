227d0e158827863eec7f6bd20d27d56396d3fbb0
wissem007 <alouiwiss@gmail.com>
2020-01-26 14:47:30 +0100

    modification code wissem2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/", (req, res) -> " DevOpsArea Java Sample Project wissem");
+		get("/", (req, res) -> " DevOpsArea Java Sample Project wissem2");
 	}
 }
